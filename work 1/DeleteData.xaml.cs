9af86f6c7f1c7e6d26402a5d7a8822bcfd2c72a7
derkor15 <162420869+derkor15@users.noreply.github.com>
2024-04-23 13:46:34 +0300

    Add files via upload

---
+++ work 1/DeleteData.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace work_1
+{
+    /// <summary>
+    /// Логика взаимодействия для DeleteData.xaml
+    /// </summary>
+    public partial class DeleteData : Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        SqlDataAdapter adapter;
+        DataTable AeroportTable;
+        SqlConnection connection = null;
+        public DeleteData()
+        {
+            InitializeComponent();
+        }
+        private void Del_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Del_txt.Text != "")
+            {
+                string sql = $"delete from Airplanes where airplane_id = {Del_txt.Text}";
+                AeroportTable = new DataTable();
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.ExecuteNonQuery();
+                    adapter = new SqlDataAdapter(command);
+                    MessageBox.Show("Данные удалены");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+
+                    if (connection != null)
+                        connection.Close();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!");
+            }
+        }
+    }
+}
